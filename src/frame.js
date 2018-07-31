8783836ac9878a8e940562975468444bd698b163
Jordan Muir <cjm712@gmail.com>
2018-07-31 00:57:16 -0400

    Use web3 < 1.0 for greater compatibility (#4)

--- src/frame.js
+++ src/frame.js
@@ -1,13 +1,14 @@
 /* global localStorage */
 
 const provider = require('eth-provider')
+const Web3 = require('web3')
 
 let active = JSON.parse(localStorage.getItem('__frameActive'))
 
 if (active) {
   try {
     const frameProvider = provider('frame')
-    window.web3 = {givenProvider: frameProvider, currentProvider: frameProvider}
+    window.web3 = new Web3(frameProvider)
   } catch (e) {
     console.error('Frame Error:', e)
   }
