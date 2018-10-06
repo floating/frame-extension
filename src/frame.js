7c794f49c4c35c85c5348869167f705a8b4816b0
C. Jordan Muir <cjm712@gmail.com>
2018-10-06 18:56:56 -0400

    Increase event listener defaults

--- src/frame.js
+++ src/frame.js
@@ -26,6 +26,7 @@ try {
     }
   }
   window.ethereum = new EthereumProvider()
+  window.ethereum.setMaxListeners(128)
   window.web3 = new Web3(window.ethereum)
 } catch (e) {
   console.error('Frame Error:', e)
