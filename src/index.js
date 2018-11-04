047ac810780474fd25686b2ad6412c59ffb04525
C. Jordan Muir <cjm712@gmail.com>
2018-11-04 00:58:09 -0400

    Clean pending/subscriptions when tab is closed

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ provider.connection.on('payload', payload => {
     if (pending[payload.id]) {
       let { tabId, payloadId } = pending[payload.id]
       if (pending[payload.id].method === 'eth_subscribe' && payload.result) {
-        subs[payload.result] = subload => chrome.tabs.sendMessage(tabId, subload)
+        subs[payload.result] = { tabId, send: subload => chrome.tabs.sendMessage(tabId, subload) }
       } else if (pending[payload.id].method === 'eth_unsubscribe') {
         payload.params.forEach(sub => delete subs[sub])
       }
@@ -23,7 +23,7 @@ provider.connection.on('payload', payload => {
       delete pending[payload.id]
     }
   } else if (payload.method && payload.method.indexOf('_subscription') > -1 && subs[payload.params.subscription]) { // Emit subscription result to tab
-    subs[payload.params.subscription](payload)
+    subs[payload.params.subscription].send(payload)
   }
 })
 
@@ -33,3 +33,16 @@ chrome.runtime.onMessage.addListener((payload, sender, sendResponse) => {
   let load = Object.assign({}, payload, { id, __frameOrigin: getOrigin(sender.url) })
   provider.connection.send(load)
 })
+
+const unsubscribeTab = tabId => {
+  Object.keys(pending).forEach(id => { if (pending[id].tabId === tabId) delete pending[id] })
+  Object.keys(subs).forEach(sub => {
+    if (subs[sub].tabId === tabId) {
+      provider.send({ jsonrpc: '2.0', id: 1, method: 'eth_unsubscribe', params: [sub] })
+      delete subs[sub]
+    }
+  })
+}
+
+chrome.tabs.onRemoved.addListener((tabId, removed) => unsubscribeTab(tabId))
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => { if (changeInfo.url) unsubscribeTab(tabId) })
