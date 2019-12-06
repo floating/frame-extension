95788575a4ebd27e6d36249d4007fdb6ba29c8a7
Jordan Muir <cjm712@gmail.com>
2019-12-06 14:47:26 -0500

    0.2 (#13)

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = [
   {
     mode: 'production',
     entry: './src/frame.js',
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: { keep_classnames: true, keep_fnames: true }
+        })
+      ]
+    },
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'frame.js'
