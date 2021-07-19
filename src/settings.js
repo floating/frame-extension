3ca910596f9e2ede4eb5f749044e659f6f9a9e65
Jordan Muir <jordan@frame.sh>
2021-07-19 00:24:34 -0400

    Polish

--- src/settings.js
+++ src/settings.js
@@ -2,18 +2,20 @@
 
 function mmAppearToggle () {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.executeScript(tabs[0].id, { code: 'localStorage[\'__mmAppear\']' }, (results) => {
-      let mmAppear = false
-      if (results) {
-        try {
-          mmAppear = JSON.parse(results[0])
-        } catch (e) {
-          mmAppear = false
+    if (tabs[0]) {
+      chrome.tabs.executeScript(tabs[0].id, { code: 'localStorage[\'__mmAppear\']' }, (results) => {
+        let mmAppear = false
+        if (results) {
+          try {
+            mmAppear = JSON.parse(results[0])
+          } catch (e) {
+            mmAppear = false
+          }
+          chrome.tabs.executeScript(tabs[0].id, { code: `localStorage.setItem('__mmAppear', ${JSON.stringify(!mmAppear)}); window.location.reload();` })
         }
-      }
-      chrome.tabs.executeScript(tabs[0].id, { code: `localStorage.setItem('__mmAppear', ${JSON.stringify(!mmAppear)}); window.location.reload();` })
-      window.close()
-    })
+        window.close()
+      })
+    }
   })
 }
 
@@ -27,10 +29,11 @@ function mmAppearToggle () {
 //   return path[0] + '//' + path[2]
 // }
 
+// document.getElementById('summonFrame').addEventListener('click', summonFrame)
+
 document.addEventListener('DOMContentLoaded', function () {
-  chrome.runtime.sendMessage({ jsonrpc: '2.0', id: 1, method: 'frame_summon', params: [] })
+  // chrome.runtime.sendMessage({ jsonrpc: '2.0', id: 1, method: 'frame_summon', params: [] })
   document.getElementById('mmAppearToggle').addEventListener('click', mmAppearToggle)
-  // document.getElementById('summonFrame').addEventListener('click', summonFrame)
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.executeScript(tabs[0].id, { code: 'localStorage[\'__mmAppear\']' }, (results) => {
       let mmAppear = false
@@ -45,12 +48,12 @@ document.addEventListener('DOMContentLoaded', function () {
       const injecting = document.getElementById('injectingAs')
       if (mmAppear) {
         toggle.className = 'frame'
-        toggle.innerHTML = 'Appear As Frame'
+        toggle.innerHTML = 'Appear As Frame Instead'
         injecting.innerHTML = 'MetaMask'
         injecting.className = 'mm'
       } else {
         toggle.className = 'mm'
-        toggle.innerHTML = 'Appear As MetaMask'
+        toggle.innerHTML = 'Appear As MetaMask Instead'
         injecting.innerHTML = 'Frame'
         injecting.className = 'frame'
       }
